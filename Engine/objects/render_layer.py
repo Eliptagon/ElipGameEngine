042aee37d3922842ef5b08c42fa6a598889fea09
Eliptagon <137432425+Eliptagon@users.noreply.github.com>
2024-07-24 22:56:11 +0100

    Delete Engine/objects directory
    Cleaning up on day 4, pt 1

--- Engine/objects/render_layer.py
+++
@@ -1,4 +0,0 @@
-from Engine.objects.base_object import engine_object
-class render_layer(engine_object):
-    def __init__(self):
-        super().__init__()
--- Engine/objects/special/sprite.py
+++
@@ -1,4 +0,0 @@
-class sprite_2d():
-    def __init__(self, location, texture):
-        self.state = 'rest'
-        self.textures = dict()
